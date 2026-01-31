188961fbeec58cd2e7bcb41cec7d7e041b676000
aowny93 <aowny93@gmail.com>
2026-01-31 03:35:01 +0000

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 console.log("Birthday wishes page loaded successfully!");
 
-// Play/pause music on click
+// Play/pause music on click anywhere
 const music = document.getElementById("bg-music");
 document.body.addEventListener("click", () => {
   if(music.paused){
